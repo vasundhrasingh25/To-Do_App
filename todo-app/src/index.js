a9452d33d987d1a7cae127618181932199082eb4
vasundhrasingh25 <vasu250300@gmail.com>
2021-12-23 23:55:00 +0530

    update index.js

--- todo-app/src/index.js
+++ todo-app/src/index.js
@@ -4,9 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { ChakraProvider } from '@chakra-ui/react';
+
 ReactDOM.render(
   <React.StrictMode>
+    <ChakraProvider>
+  <ColorModeScript initialColorMode="light" />
     <App />
+    </ChakraProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
