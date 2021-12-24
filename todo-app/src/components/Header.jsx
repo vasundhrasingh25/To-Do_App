03b7403c37d8dcb4a451c3fcbb4ac8cc94e1178d
Shreya <shreyaasinghhme@gmail.com>
2021-12-24 20:37:52 +0530

    Header.jsx updated

--- todo-app/src/components/Header.jsx
+++ todo-app/src/components/Header.jsx
@@ -1,13 +1,16 @@
-import{ Box,Heading,IconButton } from '@chakra-ui/react';
-import{ FaSun,} from'react-icons/fa';
+import{ Box,Heading,IconButton,useColorMode } from '@chakra-ui/react';
+import{ FaSun,FarMoon} from'react-icons/Fa';
+
 const Header = () =>
 {
+    const{colorMode,toggleColorMode}=useColorMode();
 return(
 <Box display ="flex" p={6}> 
     <IconButton 
-    icon={<FaSun /> }
+    icon={colorMode ==='light'?<FaSun/> :<FarMoon/>}
      isRound={true}
       mr={5}
+      onClick={toggleColorMode}
     />
     <Heading
     bgGradient='linear(to-l,red.500,yellow.500,pink.200)'
