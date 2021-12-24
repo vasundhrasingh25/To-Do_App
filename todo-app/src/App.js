e44b9b867a37c2ccf93656d79b6d76605f67a2ba
Shreya <shreyaasinghhme@gmail.com>
2021-12-24 23:23:12 +0530

    updates in AppToDo.jsx and Header.jsx

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,14 +1,19 @@
+
+import{useState}from 'react'; 
 import{VStack}from '@chakra-ui/react';
 //cpmponents
 import Header from'./components/Header';
 import AddToDo from './components/AddToDo';
 
 function App() {
+  const [todos,setTodos]=useState('');
   return (
     <VStack>
 
 <Header />
-<AddToDo />
+
+<AddToDo todos={todos}setTodos={setTodos} />
+
 
 </VStack>
     
--- todo-app/src/components/AddToDo.jsx
+++ todo-app/src/components/AddToDo.jsx
@@ -1,15 +1,36 @@
-import {HStack,Input,Button}from '@chakra-ui/react';
-const AddToDo = () =>
+
+import{useState}from 'react';
+import {HStack,Input,Button,useToast}from '@chakra-ui/react';
+const AddToDo = ({ todos, setTodo}) =>
 {
+    const [todo,setTodos]=useState(' ');
+    const  toast=useToast();
+    const handleChange= (e) => {
+        setTodo(e.target.value);
+        console.log(todo);
+    }
+    const addTodo =() =>{
+        if(!todo)return;
+        const newTodo={
+            id:11212121,
+            text:todo
+        }
+        setTodos([...todos,newTodo])
+        setTodo('')
+    }
     return(
         <HStack>
+        
             <Input
             placeholder="Add ToDo items..."
             variant="filled"
+            value={todo}
+            onChange={(e)=>handleChange(e)}
   />
   <Button        
   colorScheme="green"
   px={8}
+  onClick={() =>addToDo()}
   >
    Add Todo 
    </Button>
