31eaa904202e5f80ce1ee4d1ac0e7c115009b184
sindhukeshary14 <1929178@kiit.ac.in>
2021-12-25 12:50:03 +0530

    added todos.jsx

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,20 +1,28 @@
 
-import{useState}from 'react'; 
-import{VStack}from '@chakra-ui/react';
-//cpmponents
+import {useState} from 'react'; 
+import {VStack, Box} from '@chakra-ui/react';
+//components
 import Header from'./components/Header';
 import AddToDo from './components/AddToDo';
+import Todos from './components/Todos';
 
 function App() {
-  const [todos,setTodos]=useState('');
+  const [todos,setTodos]=useState([]);
+   
+  const deleteTodo = (id) => {
+    const updatedTodo = todos.filter(todo => todo.id !== id);
+    setTodos(updatedTodo);
+  }
   return (
-    <VStack>
-
-<Header />
-
-<AddToDo todos={todos}setTodos={setTodos} />
-
-
+<VStack>
+   <Header />
+     <Box
+       w='100%'
+       maxW={{base: '80Vw', sm: '80Vw', lg: '50Vw', xl= '40Vw'}}
+     >
+       <AddToDo todos={todos} setTodos={setTodos} />
+       <Todos todos={todos} deleteTodo={deleteTodo} />
+     </Box>
 </VStack>
     
   );
