b393496560a1b69664e88d825c87427ab4037143
sindhukeshary14 <1929178@kiit.ac.in>
2021-12-26 21:34:09 +0530

    updated EmptyTodo.jsx

--- todo-app/src/components/EmptyTodo.jsx
+++ todo-app/src/components/EmptyTodo.jsx
@@ -4,7 +4,13 @@ import {Text} from '@chakra-ui/react';
 const EmptyTodo = () =>
 {
     return(
-        <Text>No Todo items to Display </Text>
+        <Text p={5}
+        bg= 'red.400'
+        color='white'
+        borderRadius={10}
+        textAlign={'center'}
+        fontSize={20}
+        >No Todo items to Display </Text>
 
     )
 }
